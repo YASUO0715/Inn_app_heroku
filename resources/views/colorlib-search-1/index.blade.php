7873783ef53f3e458dc5b0d7782835c566689ffd
YASUO0715 <yasuo0715niihori@gmail.com>
2021-11-15 11:40:07 +0900

    トップページ迄の背景画像修正

--- resources/views/colorlib-search-1/index.blade.php
+++ resources/views/colorlib-search-1/index.blade.php
@@ -6,22 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="author" content="colorlib.com">
-    <link rel="stylesheet" href="{{ asset('images/nihon_bashi.jpeg') }}">
+    <link rel="stylesheet" href="{{ asset('images/Sample.png') }}">
     <link href="css/main.css" rel="stylesheet" />
   </head>
   <body>
     <div class="s01">
       <form>
         <fieldset>
-          <legend><b>最安値で、今夜のお宿を</b></legend>
+          {{-- <legend><b>最安値で、今夜のお宿を</b></legend> --}}
         </fieldset>
         <div class="inner-form">
           <div class="input-field first-wrap">
-            <input id="search" type="text" placeholder="何をお探しですか" />
+            <input id="search" type="text" placeholder="現在地から検索" />
           </div>
-          <div class="input-field second-wrap">
+          {{-- <div class="input-field second-wrap">
             <input id="location" type="text" placeholder="現在地から検索" />
-          </div>
+          </div> --}}
           <div class="input-field third-wrap">
             <button class="btn-search" type="button">Search</button>
           </div>
